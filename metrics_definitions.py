63c2f353cc27f2d60ebed7e26fa0e924bad83924
Josaarij <jussi.saarijarvi@gmail.com>
2026-03-02 12:59:22 +0200

    Create metrics_definitions.py

---
+++ metrics_definitions.py
@@ -0,0 +1,26 @@
+ALL_METRICS = {
+    "ELINVOIMA": [
+        "Pelaajamäärä yht.",
+        "Nettokasvu (uudet–lopettaneet)",
+        "Lopettamis-% 13–15v",
+        "Tyttö-/naispelaajamäärä",
+    ],
+    "TALOUS": [
+        "Kassatilanne + ennuste",
+        "Tulosennuste",
+        "Kattavuus % (maksut/kulut)",
+        "Muut tuotot",
+    ],
+    "VALMENNUS": [
+        "Valmentajien pysyvyys",
+        "Koulutetut %",
+        "Valmentajamäärä/joukkue",
+    ],
+    "LAATU": [
+        "Pelaajatyytyväisyys",
+        "Vanhempien tyytyväisyys",
+        "Valmentajien/taustojen tyytyväisyys",
+        "Huipputasolle nousseet/vuosi",
+        "Valmennuslinjan toteutuminen",
+    ],
+}
