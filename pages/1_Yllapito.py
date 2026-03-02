f5697d80020d9b41b6714d0af6b49acc0baeea2f
Josaarij <jussi.saarijarvi@gmail.com>
2026-03-02 13:04:40 +0200

    Update 1_Yllapito.py

--- pages/1_Yllapito.py
+++ pages/1_Yllapito.py
@@ -30,32 +30,7 @@ def get_status(value, target, warning, direction):
 
 
 # --- Mittarit (kiinteästi hallituksen päättämät) ---
-ALL_METRICS = {
-    "ELINVOIMA": [
-        "Pelaajamäärä yht.",
-        "Nettokasvu (uudet–lopettaneet)",
-        "Lopettamis-% 13–15v",
-        "Tyttö-/naispelaajamäärä",
-    ],
-    "TALOUS": [
-        "Kassatilanne + ennuste",
-        "Tulosennuste",
-        "Kattavuus % (maksut/kulut)",
-        "Muut tuotot",
-    ],
-    "VALMENNUS": [
-        "Valmentajien pysyvyys",
-        "Koulutetut %",
-        "Valmentajamäärä/joukkue",
-    ],
-    "LAATU": [
-        "Pelaajatyytyväisyys",
-        "Vanhempien tyytyväisyys",
-        "Valmentajien/taustojen tyytyväisyys",
-        "Huipputasolle nousseet/vuosi",
-        "Valmennuslinjan toteutuminen",
-    ],
-}
+from metrics_definitions import ALL_METRICS
 
 # --- Oletusarvot (voit muokata myöhemmin) ---
 DEFAULTS = {
